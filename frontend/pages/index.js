6543b10cec10ab6dfb5fa02468d88bfa95851ba6
Mayur2801 <100698001+Mayur2801@users.noreply.github.com>
2025-06-18 11:23:21 +0530

    Add files via upload

---
+++ frontend/pages/index.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Next.js Frontend</h1>
+      <p>Message from backend: {message}</p>
+    </div>
+  );
+}
+
